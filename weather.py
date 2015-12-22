637ab825d9932ce4120fce61291af79ab79b3094
James <jbkuczma@buffalo.edu>
2015-12-22 10:07:56 -0500

    various improvements with program. 5 day forecast added

--- weather.py
+++ weather.py
@@ -1,8 +1,26 @@
 import pywapi
-import string
 
 city_ = ""
 zip_ = ""
+temperature_ = ""
+conditions_ = ""
+cityID_ = ""
+
+
+def checkIfValidZip(choice):
+    global zip_
+    f = open("all_zip_codes.txt", "r")
+    found = False
+    for line in f.readlines():
+        line = line.strip().lower()
+        if str(line) == str(choice):
+            print("valid")
+            found = True
+            zip_ = choice
+            break
+    if (found == False):
+        choice = input("You did not enter a valid zip code. Please try again: ")
+        checkIfValidZip(choice)
 
 
 def getChoice():
@@ -13,6 +31,7 @@ def getChoice():
         return city_
     elif int(choice) == 2:
         zip_ = input("Please enter a zip code: ")
+        checkIfValidZip(zip_)
         return zip_
     else:
         print("Your input is not a valid option. Please try again.")
@@ -20,33 +39,65 @@ def getChoice():
 
 
 def getWeather():
+    global cityID_, zip_, city_, temperature_, conditions_
     if city_ != "":
         city = pywapi.get_location_ids(city_)
         cityID = list(city.keys())
-        print("city: " + str(city))
-        print("val id: " + str(cityID))
+        tmp = list(city.values())
+        city_ = tmp[0]
+        cityID_ = cityID
         result = pywapi.get_weather_from_weather_com(cityID[0])
-        # print("It is " + result["current_conditions"]["text"].lower())
-        print("It is " + str(result['current_conditions']['text']) + " and " + result['current_conditions']['temperature'] + "°C now in " + city_ + ".\n\n")
+        conditions_ = str(result['current_conditions']['text'])
+        temperature_ = float(result['current_conditions']['temperature']) * 1.8 + 32
     else:
         newCity = ""
         zip = pywapi.get_location_ids(zip_)
-        # print("zip: " + str(zip))
         cityName = list(zip.values())
-        # print("r: " + result[0])
         for i in cityName[0]:
             if not i.isdigit() and i != '(' and i != ')':
                 newCity += i
-        # print("p: " + newCity)
+        city_ = newCity
         cityList = pywapi.get_location_ids(newCity)
-        # print("t: " + str(cityList))
         cityID = list(cityList.keys())
+        cityID_ = cityID
         result = pywapi.get_weather_from_weather_com(cityID[0])
-        print("It is " + str(result['current_conditions']['text']) + " and " + result['current_conditions']['temperature'] + "°C now in " + newCity + ".\n\n")
+        conditions_ = str(result['current_conditions']['text'])
+        temperature_ = float(result['current_conditions']['temperature']) * 1.8 + 32
 
 
+def getWeekOfWeather():
+    choice = input("Would you like to see the forecast for the next five days(1) or exit?")
+    printWeather(choice)
+
+
+def printWeather(choice):
+    if str(choice) != str(1):
+        print("-----------------------------------------------")
+        print("City: " + str(city_))
+        print("Zip: " + str(zip_))
+        print("Temperature: " + str(temperature_) + "°F")
+        print("Conditions: " + str(conditions_))
+        print("-----------------------------------------------")
+        exit(0)
+    else:
+        result = pywapi.get_weather_from_weather_com(str(cityID_[0]))
+        print("----------- Current Conditions ----------------")
+        print("City: " + str(city_))
+        print("Zip: " + str(zip_))
+        print("Temperature: " + str(temperature_) + "°F")
+        print("Conditions: " + str(conditions_) + '\n')
+        print("----------- 5 Day Forecast --------------------")
+        for i in range(0, 5):
+            date = str(result['forecasts'][i]['date'])
+            dayofweek = str(result['forecasts'][i]['day_of_week'])
+            conditions = str(result['forecasts'][i]['day']['brief_text'])
+            high = str(float(result['forecasts'][i]['high']) * 1.8 + 32)
+            low = str(float(result['forecasts'][i]['low']) * 1.8 + 32)
+            print(dayofweek + "," + date + ": High: " + high + "°F Low: " + low + "°F Forecast: " + conditions)
+        print("-----------------------------------------------")
 
 
 if __name__ == '__main__':
     getChoice()
     getWeather()
+    getWeekOfWeather()
